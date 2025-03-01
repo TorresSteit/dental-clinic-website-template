adb8ca081dd533fe3a3e8b2f18d75a7104a4df8b
Torres <taras.royale@gmail.com>
2025-03-01 17:55:03 +0100

    translate1

--- js/translete.js
+++ js/translete.js
@@ -8,22 +8,58 @@ const translations = {
         team: "Náš tím",
         reviews: "Recenzie",
         contact: "Kontakt",
+        acute_patients: "OŠETRUJEME AJ AKÚTNYCH PACIENTOV, KTORÍ NIE SÚ DOTERAZ V NAŠEJ EVIDENCII. ZAVOLAJTE A DOHODNIEME SA O TERMÍN",
         call_us: "Zavolajte nám",
+        acute_patients1: "OŠETRUJEME AJ AKÚTNYCH PACIENTOV, KTORÍ NIE SÚ DOTERAZ V NAŠEJ EVIDENCII. ZAVOLAJTE A DOHODNIEME SA O TERMÍN",
+        call_us1: "Zavolajte nám",
         welcome: "Udržujte svoje zuby zdravé",
         header: "Získajte najkvalitnejšiu zubnú starostlivosť",
         opening_hours: "Otváracie hodiny",
         monday: "Pondelok",
+        monday_hours: "8:00 - 15:00",
         tuesday: "Utorok",
+        tuesday_hours: "12:00 - 20:00",
         wednesday: "Streda",
+        wednesday_hours: "8:00 - 15:00",
         thursday: "Štvrtok",
+        thursday_hours: "12:00 - 20:00",
         friday: "Piatok",
+        friday_hours: "8:00 - 15:00",
         saturday: "Sobota",
-        sunday: "Nidela",
+        saturday_hours: "Na objednávku",
+        sunday: "Nedeľa",
+        sunday_hours: "Nepracujeme",
         our_services: "Naše služby",
         best_dental_care: "Ponúkame najkvalitnejšie zubné služby",
         cosmetic_dentistry: "Kozmetická stomatológia",
         teeth_whitening: "Bielenie zubov",
-        fair_prices: "Ponúkame férové ceny za zubnú starostlivosť"
+        fair_prices: "Ponúkame férové ceny za zubnú starostlivosť",
+        titlechild:"𝑫𝒆𝒕𝒔𝒌á 𝒔𝒕𝒐𝒎𝒂𝒕𝒐𝒍ó𝒈𝒊𝒂",
+        listchild1:"Rozpravky počas osetrenia",
+        listchild2:"Darčeky a diplomy",
+        listchild3:"Osetrenie v spánku",
+        titlegigenia:"𝑯𝒚𝒈𝒊𝒆𝒏𝒂 𝒂 𝒑𝒓𝒆𝒗𝒆𝒏𝒄𝒊𝒂",
+        listgigenia1:"Stomatologický Check-Up",
+        listgigenia2:"Fluorizácia zubov",
+        listgigenia3:"Hygienické indikátory",
+        titlemezoterapia:"𝑀𝑒𝑧𝑜𝑡𝑒𝑟𝑎𝑝𝑖𝑎 𝑝𝑜𝑑á𝑣𝑎𝑛á 𝑖𝑛𝑗𝑒𝑘č𝑛𝑒",
+        listmezoterapia1:"1ml kyseliny hyaluronovej",
+        listmezoterapia2:"2ml kyseliny hyaluronovej",
+        listmezoterapia3:"3ml kyseliny hyaluronovej",
+        titleprotetika:"𝑷𝒓𝒐𝒕𝒆𝒕𝒊𝒌𝒂",
+        listprotetika1:"Stomatologicky check up",
+        listprotetika2:"Fluoridacia a bielenie zubov",
+        listprotetika3:"Dentalna hygiena a prevencia kazu",
+        listprotetika4:"Zirkonove a kerwmicke korunky",
+        titlechirgia:"𝑪𝒉𝒊𝒓𝒖𝒓𝒈𝒊𝒂",
+        listchirgia1:"Extrakcia trhanie mliečneho zuba",
+        listchirgia2:"Extrakcia trhanie trvalého jednokoreňového zuba",
+        listchirgia3:"Extrakcia trhanie trvalého viackoreňového zuba",
+        titleterpia:"𝑻𝒆𝒓𝒂𝒑𝒊𝒂",
+        listterpia1:"Prevencia kazu",
+        listterpia2:"Osetrenie zubov a sliznice",
+        listterpia3:"Osetrenie korenovych kanalikov",
+
     },
     ru: {
         home: "Главная",
@@ -34,22 +70,60 @@ const translations = {
         team: "Наша команда",
         reviews: "Отзывы",
         contact: "Контакты",
+        acute_patients: "МЫ ПРИНИМАЕМ ОСТРЫХ ПАЦИЕНТОВ, КОТОРЫХ ЕЩЁ НЕТ В НАШЕЙ БАЗЕ. ПОЗВОНИТЕ, И МЫ ДОГОВОРИМСЯ О ВРЕМЕНИ",
         call_us: "Позвоните нам",
+        acute_patients1: "МЫ ПРИНИМАЕМ ОСТРЫХ ПАЦИЕНТОВ, КОТОРЫХ ЕЩЁ НЕТ В НАШЕЙ БАЗЕ. ПОЗВОНИТЕ, И МЫ ДОГОВОРИМСЯ О ВРЕМЕНИ",
+        call_us1: "Позвоните нам",
         welcome: "Берегите свои зубы",
         header: "Получите лучший стоматологический уход",
         opening_hours: "Часы работы",
         monday: "Понедельник",
+        monday_hours: "8:00 - 15:00",
         tuesday: "Вторник",
+        tuesday_hours: "12:00 - 20:00",
         wednesday: "Среда",
+        wednesday_hours: "8:00 - 15:00",
         thursday: "Четверг",
+        thursday_hours: "12:00 - 20:00",
         friday: "Пятница",
-        saturday: "Субота",
-        sunday: "Воскресения",
+        friday_hours: "8:00 - 15:00",
+        saturday: "Суббота",
+        saturday_hours: "По заказу",
+        sunday: "Воскресенье",
+        sunday_hours: "Не работаем",
         our_services: "Наши услуги",
         best_dental_care: "Мы предлагаем лучшие стоматологические услуги",
         cosmetic_dentistry: "Косметическая стоматология",
         teeth_whitening: "Отбеливание зубов",
-        fair_prices: "Мы предлагаем честные цены на стоматологические услуги"
+        fair_prices: "Мы предлагаем честные цены на стоматологические услуги",
+        titlechild: "Детская стоматология",
+        listchild1: "Сказки во время лечения",
+        listchild2: "Подарки и дипломы",
+        listchild3: "Лечение во сне",
+        titlegigenia: "Гигиена и профилактика",
+        listgigenia1: "Стоматологический Check-Up",
+        listgigenia2: "Фторирование зубов",
+        listgigenia3: "Гигиенические показатели",
+        titlemezoterapia:"Мезотерапия инъекционная",
+        listmezoterapia1:"1 мл гиалуроновой кислоты",
+        listmezoterapia2:"2 мл гиалуроновой кислоты",
+        listmezoterapia3:"3 мл гиалуроновой кислоты",
+        titleprotetika:"Протезирование",
+        listprotetika1:"Стоматологический check-up",
+        listprotetika2:"Фторирование и отбеливание зубов",
+        listprotetika3:"Дентальная гигиена и профилактика кариеса",
+        listprotetika4:"Циркониевые и керамические коронки",
+        titlechirgia:"Хирургия",
+        listchirgia1:"Extrakcia trhanie mliečneho zuba",
+        listchirgia2:"Extrakcia trhanie trvalého jednokoreňového zuba",
+        listchirgia3:"Extrakcia trhanie trvalého viackoreňového zuba",
+        titleterpia:"Терапия",
+        listterpia1:"Профилактика кариеса",
+        listterpia2:"Лечение зубов и слизистой",
+        listterpia3:"Лечение корневых каналов",
+
+
+
     },
     uk: {
         home: "Головна",
@@ -60,22 +134,60 @@ const translations = {
         team: "Наша команда",
         reviews: "Відгуки",
         contact: "Контакти",
+        acute_patients: "МИ ПРИЙМАЄМО ГОСТРИХ ПАЦІЄНТІВ, ЯКИХ ЩЕ НЕМАЄ В НАШІЙ БАЗІ. ЗАТЕЛЕФОНУЙТЕ, І МИ ДОМОВИМОСЯ ПРО ЧАС",
         call_us: "Зателефонуйте нам",
+        acute_patients1: "МИ ПРИЙМАЄМО ГОСТРИХ ПАЦІЄНТІВ, ЯКИХ ЩЕ НЕМАЄ В НАШІЙ БАЗІ. ЗАТЕЛЕФОНУЙТЕ, І МИ ДОМОВИМОСЯ ПРО ЧАС",
+        call_us1: "Зателефонуйте нам",
         welcome: "Бережіть свої зуби",
         header: "Отримайте найкращий стоматологічний догляд",
         opening_hours: "Години роботи",
         monday: "Понеділок",
+        monday_hours: "8:00 - 15:00",
         tuesday: "Вівторок",
+        tuesday_hours: "12:00 - 20:00",
         wednesday: "Середа",
+        wednesday_hours: "8:00 - 15:00",
         thursday: "Четвер",
+        thursday_hours: "12:00 - 20:00",
         friday: "П’ятниця",
+        friday_hours: "8:00 - 15:00",
         saturday: "Субота",
+        saturday_hours: "За домовленістю",
         sunday: "Неділя",
+        sunday_hours: "Не працюємо",
         our_services: "Наші послуги",
         best_dental_care: "Ми пропонуємо найкращі стоматологічні послуги",
         cosmetic_dentistry: "Косметична стоматологія",
         teeth_whitening: "Відбілювання зубів",
-        fair_prices: "Ми пропонуємо чесні ціни на стоматологічні послуги"
+        fair_prices: "Ми пропонуємо чесні ціни на стоматологічні послуги",
+        titlechild: "Дитяча стоматологія",
+        listchild1: "Казки під час лікування",
+        listchild2: "Подарунки та дипломи",
+        listchild3: "Лікування уві сні",
+        titlegigenia: "Гігієна та профілактика",
+        listgigenia1: "Стоматологічний Check-Up",
+        listgigenia2: "Фторування зубів",
+        listgigenia3: "Гігієнічні показники",
+        titlemezoterapia:"Ін'єкційна мезотерапія",
+        listmezoterapia1:"1 мл гіалуронової кислоти",
+        listmezoterapia2:"2 мл гіалуронової кислоти",
+        listmezoterapia3:"3 мл гіалуронової кислоти",
+        titleprotetika:"Протезування",
+        listprotetika1:"Стоматологічний check-up",
+        listprotetika2:"Фторування та відбілювання зубів",
+        listprotetika3:"Дентальна гігієна та профілактика карієсу",
+        listprotetika4:"Цирконієві та керамічні коронки",
+        titlechirgia:"Хірургія",
+        listchirgia1:"Extrakcia trhanie mliečneho zuba",
+        listchirgia2:"Extrakcia trhanie trvalého jednokoreňového zuba",
+        listchirgia3:"Extrakcia trhanie trvalého viackoreňového zuba",
+        titleterpia:"Терапія",
+        listterpia1:"Профілактика карієсу",
+        listterpia2:"Лікування зубів і слизової",
+        listterpia3:"Лікування кореневих каналів",
+
+
+
     },
     en: {
         home: "Home",
@@ -86,22 +198,59 @@ const translations = {
         team: "Our Team",
         reviews: "Reviews",
         contact: "Contact",
+        acute_patients: "WE ALSO TREAT ACUTE PATIENTS WHO ARE NOT YET IN OUR RECORDS. CALL US TO SCHEDULE AN APPOINTMENT",
         call_us: "Call Us",
+        acute_patients1: "WE ALSO TREAT ACUTE PATIENTS WHO ARE NOT YET IN OUR RECORDS. CALL US TO SCHEDULE AN APPOINTMENT",
+        call_us1: "Call Us",
         welcome: "Keep Your Teeth Healthy",
         header: "Get the Best Dental Care",
         opening_hours: "Opening Hours",
         monday: "Monday",
+        monday_hours: "8:00 - 15:00",
         tuesday: "Tuesday",
+        tuesday_hours: "12:00 - 20:00",
         wednesday: "Wednesday",
+        wednesday_hours: "8:00 - 15:00",
         thursday: "Thursday",
+        thursday_hours: "12:00 - 20:00",
         friday: "Friday",
+        friday_hours: "8:00 - 15:00",
         saturday: "Saturday",
+        saturday_hours: "By Appointment",
         sunday: "Sunday",
+        sunday_hours: "Closed",
         our_services: "Our Services",
         best_dental_care: "We Offer the Best Dental Services",
         cosmetic_dentistry: "Cosmetic Dentistry",
         teeth_whitening: "Teeth Whitening",
-        fair_prices: "We Offer Fair Prices for Dental Care"
+        fair_prices: "We Offer Fair Prices for Dental Care",
+        titlechild: "Pediatric Dentistry",
+        listchild1: "Fairy Tales During Treatment",
+        listchild2: "Gifts and Diplomas",
+        listchild3: "Treatment Under Anesthesia",
+        titlegigenia: "Hygiene and Prevention",
+        listgigenia1: "Dental Check-Up",
+        listgigenia2: "Fluoride Treatment",
+        listgigenia3: "Hygiene Indicators",
+        titlemezoterapia:"Injectable Mesotherapy",
+        listmezoterapia1:"1ml Hyaluronic Acid",
+        listmezoterapia2:"2ml Hyaluronic Acid",
+        listmezoterapia3:"3ml Hyaluronic Acid",
+        titleprotetika:"Prosthetics",
+        listprotetika1:"Dental Check-Up",
+        listprotetika2:"Fluoridation and teeth whitening",
+        listprotetika3:"Dental hygiene and caries prevention",
+        listprotetika4:"Zirconia and ceramic crowns",
+        titlechirgia:"Surgery",
+        listchirgia1:"Extrakcia trhanie mliečneho zuba",
+        listchirgia2:"Extrakcia trhanie trvalého jednokoreňového zuba",
+        listchirgia3:"Extrakcia trhanie trvalého viackoreňového zuba",
+        titleterpia:"Therapy",
+        listterpia1:"Caries prevention",
+        listterpia2:"Teeth and mucous membrane treatment",
+        listterpia3:"ORoot canal treatment",
+
+
     }
 };
 
@@ -111,27 +260,105 @@ document.getElementById("ukBtn").addEventListener("click", () => switchLanguage(
 document.getElementById("enBtn").addEventListener("click", () => switchLanguage("en"));
 
 function switchLanguage(lang) {
-    document.querySelector(".nav-item.nav-link.active").textContent = translations[lang].home;
-    document.querySelector(".nav-item.nav-link[href='about.html']").textContent = translations[lang].about;
-    document.querySelector(".nav-item.nav-link[href='service.html']").textContent = translations[lang].services;
-    document.querySelector(".nav-link.dropdown-toggle").textContent = translations[lang].pages;
-    document.querySelector(".dropdown-item[href='price.html']").textContent = translations[lang].pricing;
-    document.querySelector(".dropdown-item[href='team.html']").textContent = translations[lang].team;
-    document.querySelector(".dropdown-item[href='testimonial.html']").textContent = translations[lang].reviews;
-    document.querySelector(".nav-item.nav-link[href='contact.html']").textContent = translations[lang].contact;
-    document.querySelector(".btn[href^='tel']").textContent = translations[lang].call_us;
-    document.querySelector(".carousel-caption h5").textContent = translations[lang].welcome;
-    document.querySelector(".carousel-caption h1").textContent = translations[lang].header;
-    document.querySelector(".carousel-caption h5").textContent = translations[lang].reviews;
+    console.log(`Switching to language: ${lang}`);
+    localStorage.setItem('language', lang); // Save selected language
+    updateTexts(lang);
+    updateActiveButton(lang);
+}
 
-    document.querySelectorAll(".carousel-caption h5").forEach(el => el.textContent = translations[lang].welcome);
-    document.querySelectorAll(".carousel-caption h1").forEach(el => el.textContent = translations[lang].header);
-    document.querySelectorAll(".btn[href^='tel']").forEach(el => el.textContent = translations[lang].call_us);
+function updateTexts(lang) {
+    console.log(`Updating texts for language: ${lang}`);
+
+    const textElements = [
+        {selector: ".nav-item.nav-link.active", text: translations[lang].home},
+        {selector: ".nav-item.nav-link[href='about.html']", text: translations[lang].about},
+        {selector: ".nav-item.nav-link[href='service.html']", text: translations[lang].services},
+        {selector: ".nav-link.dropdown-toggle", text: translations[lang].pages},
+        {selector: ".dropdown-item[href='price.html']", text: translations[lang].pricing},
+        {selector: ".dropdown-item[href='team.html']", text: translations[lang].team},
+        {selector: ".dropdown-item[href='testimonial.html']", text: translations[lang].reviews},
+        {selector: ".nav-item.nav-link[href='contact.html']", text: translations[lang].contact},
+        {selector: ".btn[href^='tel']", text: translations[lang].call_us},
+        {selector: ".display-1", text: translations[lang].acute_patients},
+        {selector: ".display-2", text: translations[lang].acute_patients1},
+        {selector: ".carousel-caption h5", text: translations[lang].welcome},
+        {selector: ".carousel-caption h1", text: translations[lang].header},
+        {selector: "h3.text-white.mb-3", text: translations[lang].opening_hours},
+        {selector: "h4.text-white.mb-3", text: translations[lang].contact},
+        {selector: ".service-card h3", text: translations[lang].titlechild},
+        {selector: ".service-card ul li:nth-child(1)", text: translations[lang].listchild1},
+        {selector: ".service-card ul li:nth-child(2)", text: translations[lang].listchild2},
+        {selector: ".service-card ul li:nth-child(3)", text: translations[lang].listchild3},
+        {selector: "#titlegigenia", text: translations[lang].titlegigenia},
+        {selector: "#listgigenia1", text: translations[lang].listgigenia1},
+        {selector: "#listgigenia2", text: translations[lang].listgigenia2},
+        {selector: "#listgigenia3", text: translations[lang].listgigenia3},
+        {selector: "#titlemezoterapia", text: translations[lang].titlemezoterapia},
+        {selector: "#listmezoterapia1", text: translations[lang].listmezoterapia1},
+        {selector: "#listmezoterapia2", text: translations[lang].listmezoterapia2},
+        {selector: "#listmezoterapia3",text: translations[lang].listmezoterapia3},
+        {selector: "#titleprotetika",text: translations[lang].titleprotetika},
+        {selector: "#listprotetika1", text: translations[lang].listprotetika1},
+        {selector: "#listprotetika2", text: translations[lang].listprotetika2},
+        {selector: "#listprotetika3", text: translations[lang].listprotetika3},
+        {selector: "#listprotetika4", text: translations[lang].listprotetika4},
+        {selector: "#titlechirgia", text: translations[lang].titlechirgia,},
+
+        {selector: "#titleterpia",text: translations[lang].titleterpia},
+        {selector: "#listterpia1", text: translations[lang].listterpia1},
+        {selector: "#listterpia2", text: translations[lang].listterpia2},
+        {selector: "#listterpia3", text: translations[lang].listterpia3},
+
+
+
+    ];
+
+    // Updating the text content for each element
+    textElements.forEach(el => {
+        const element = document.querySelector(el.selector);
+        if (element) {
+            element.textContent = el.text;
+        } else {
+            console.log(`Element not found for selector: ${el.selector}`);
+        }
+    });
+
+    // Update work hours for each day
     const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
     document.querySelectorAll(".d-flex h6").forEach((el, index) => {
-        el.textContent = translations[lang][days[index]];
+        if (el) {
+            el.textContent = translations[lang][days[index]];
+        } else {
+            console.log(`Element not found for day: ${days[index]}`);
+        }
     });
-    document.querySelector("h3.text-white.mb-3").textContent = translations[lang].opening_hours;
 
-
+    // Update hours for each day
+    document.querySelectorAll(".d-flex p.mb-0").forEach((el, index) => {
+        if (el) {
+            el.textContent = translations[lang][days[index] + "_hours"];
+        } else {
+            console.log(`Element not found for day hours: ${days[index]}`);
+        }
+    });
 }
+
+function updateActiveButton(lang) {
+    console.log(`Updating active button for language: ${lang}`);
+    document.querySelectorAll('button').forEach(button => {
+        button.classList.remove('active');
+    });
+    const activeButton = document.getElementById(lang + 'Btn');
+    if (activeButton) {
+        activeButton.classList.add('active');
+    } else {
+        console.log(`Button not found for language: ${lang}`);
+    }
+}
+
+// Restore the language on page load
+window.addEventListener("load", () => {
+    const savedLang = localStorage.getItem('language') || 'en'; // default to English
+    switchLanguage(savedLang);
+});
+
